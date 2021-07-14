5c588c63acc7fd96049ddb0e03f6e6aa972e170b
vignesh-82coder <61943147+vignesh-82coder@users.noreply.github.com>
2021-07-14 23:12:35 +0530

    Add files via upload

---
+++ DSxFFP-main/js/music-list.js
@@ -0,0 +1,142 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Sen -A Journey",
+    artist: "Vignesh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Vande Mataram",
+    artist: "Vignesh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Latika's Theme -cover",
+    artist: "Vignesh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Old town road- Indian cover",
+    artist: "Kevin Noel",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ratsasan Theme -cover",
+    artist: "Vignesh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name:"Blinding lights -cover" ,
+    artist: "Vignesh",
+    img: "music-6",
+    src: "music-6"
+  },
+  ///////
+  {
+    name: "Mayakkam Enna theme -cover",
+    artist: "Kevin x Vignesh",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Nucleya's Mumbai Dance -cover",
+    artist: "Kevin Noel",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "On My Way -Beatboxed",
+    artist: "Vignesh x Sangmesh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+      name: "Among Us- Indian cover",
+      artist: "Kevin Noel",
+      img: "music-10",
+      src: "music-10"
+    },
+    {
+        name: "7 Rings -Indian version cover",
+        artist: "Vignesh",
+        img: "music-11",
+        src: "music-11"
+      },
+      {
+          name: "Believer -cover",
+          artist: "Kevin Noel",
+          img: "music-12",
+          src: "music-12"
+        },
+        {
+            name: "Arjun Reddy / Kabir Singh Theme -cover",
+            artist: "Kevin Noel",
+            img: "music-13",
+            src: "music-13"
+          },
+          {
+              name: "Master theme -cover",
+              artist: "Vignesh",
+              img: "music-14",
+              src: "music-14"
+            },
+            {
+                  name: "The Scam 1992 theme -cover",
+                  artist: "Kevin Noel",
+                  img: "music-15",
+                  src: "music-15"
+                },
+                {
+                      name: "Three Bike BGM -cover",
+                      artist: "Vignesh",
+                      img: "music-16",
+                      src: "music-16"
+                    },
+                    {
+                          name: "Bad guy -cover",
+                          artist: "Vignesh",
+                          img: "music-17",
+                          src: "music-17"
+                        },
+                         {
+                                name: "Pyaar Prema Kaadhal Theme -cover",
+                                artist: "Vignesh",
+                                img: "music-18",
+                                src: "music-18"
+                              },
+                              {
+                                     name: "Thanmayaladenu -cover",
+                                     artist: "Savitha Satish",
+                                     img: "music-19",
+                                     src: "music-19"
+                                   },
+                                   {
+                                          name: "Thendral Vandhu -cover",
+                                          artist: "Vignesh",
+                                          img: "music-20",
+                                          src: "music-20"
+                                        },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
